812d0ae5fc10da39a850870f44ff0b96b88d5644
Tom Huynh <nghia.huynh@utexas.edu>
2017-09-14 21:00:53 -0500

    make turtle move in random path instead of circle

--- scripts/vel_generator.py
+++ scripts/vel_generator.py
@@ -5,15 +5,20 @@
 
 import rospy
 from geometry_msgs.msg import Twist, Vector3
+import random
 
 def velocity_generator():
     pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)
     rospy.init_node('vel_generator', anonymous=True)
-    rate = rospy.Rate(1) # 1hz
+    rate = rospy.Rate(10) # 1hz
     while not rospy.is_shutdown():
         #hello_str = "hello world %s" % rospy.get_time()
-        linear = Vector3(2.0, 0.0, 0.0)
-        angular = Vector3(0.0, 0.0, 1.8)
+        if random.random() < 0.5:
+            sign = -1.0
+        else:
+            sign = 1.0
+        linear = Vector3(random.random() * 3, 0.0, 0.0)
+        angular = Vector3(0.0, 0.0, random.random() * 2 * 3.14 * sign)
         ve = Twist(linear, angular)
         rospy.loginfo(str(ve))
         pub.publish(ve)
