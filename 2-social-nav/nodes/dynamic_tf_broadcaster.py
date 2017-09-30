60513596aecae010629fe8c80d41119380adcd7a
Tom Huynh <nghia.huynh@utexas.edu>
2017-09-30 18:51:26 -0500

    implemented periodic proxemic changes to simulate mood add README for social nav

--- 2-social-nav/nodes/dynamic_tf_broadcaster.py
+++ 2-social-nav/nodes/dynamic_tf_broadcaster.py
@@ -10,9 +10,20 @@ if __name__ == '__main__':
     rospy.init_node('dynamic_tf_broadcaster')
     br = tf.TransformBroadcaster()
     rate = rospy.Rate(10.0)
+    mood = 2.5
+    sign = 1.0
+    prev_t = rospy.Time.now().to_sec()
     while not rospy.is_shutdown():
-        t = rospy.Time.now().to_sec() * math.pi
-        br.sendTransform((2.0 * math.sin(t), 2.0 * math.cos(t), 0.0),
+        t = rospy.Time.now().to_sec()
+        if t - prev_t > 2:
+            prev_t = t
+            if mood > 3:
+                sign = -1.0
+            elif mood < 1:
+                sign = 1.0
+            mood = mood + sign*1
+        #br.sendTransform((2.0 * math.sin(t), 2.0 * math.cos(t), 0.0),
+        br.sendTransform((0.0, mood, 0.0),
                          (0.0, 0.0, 0.0, 1.0),
                          rospy.Time.now(),
                          "carrot1",
